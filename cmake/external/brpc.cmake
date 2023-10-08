ae53e7bde674ef9fa8326927ea22ec644015aafc
fanghuaqiang <vivienfanghua@163.com>
2023-10-08 21:26:02 +0800

    add

--- cmake/external/brpc.cmake
+++ cmake/external/brpc.cmake
@@ -48,10 +48,11 @@ set(prefix_path
     "${THIRD_PARTY_PATH}/install/gflags|${THIRD_PARTY_PATH}/install/leveldb|${THIRD_PARTY_PATH}/install/snappy|${THIRD_PARTY_PATH}/install/gtest|${THIRD_PARTY_PATH}/install/protobuf|${THIRD_PARTY_PATH}/install/zlib|${THIRD_PARTY_PATH}/install/glog"
 )
 
-file(TO_NATIVE_PATH ${PADDLE_SOURCE_DIR}/patches/brpc/rep.patch native_src)
+file(TO_NATIVE_PATH ${PADDLE_SOURCE_DIR}/patches/brpc/a.patch all_patch_path)
 set(BRPC_PATCH_COMMAND
-    git checkout -- . && git checkout ${BRPC_TAG} && patch -Nd
-    ${PADDLE_SOURCE_DIR}/third_party/brpc/src/bthread/ < ${native_src})
+    git checkout -- . && git checkout ${BRPC_TAG} && 
+    git apply ${all_patch_path}
+    )
 
 set(BRPC_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -faligned-new -Wno-error=misleading-indentation -Wno-error=parentheses -Wno-error=implicit-fallthrough -D__const__=__unused__ -std=c++11")
 # If minimal .a is need, you can set  WITH_DEBUG_SYMBOLS=OFF
